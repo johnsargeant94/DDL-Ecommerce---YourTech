e09367dcd46601829d1eb676abf1c55d0c447b78
John Sargeant <71139230+johnsargeant94@users.noreply.github.com>
2020-11-09 18:31:15 +0000

    basket.JS

--- public/basket.js
+++ public/basket.js
@@ -1,1 +1,35 @@
-console.log("basket js file working");
+let carts = document.querySelectorAll('#basketBTN');
+
+
+
+for (let i=0; i < carts.length; i++){
+    carts[i].addEventListener('click', () => {
+        cartNumbers();
+
+    })
+}
+
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if(productNumbers) {
+        document.querySelector('basketNum').textContent = productNumbers;
+    }
+}
+
+function cartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    productNumbers = parseInt(productNumbers);
+
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers' , productNumbers + 1);
+        document.querySelector('basketNum').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers' , 1);
+        document.querySelector('basketNum').textContent = 1;
+    }
+
+}
+
+onLoadCartNumbers()
