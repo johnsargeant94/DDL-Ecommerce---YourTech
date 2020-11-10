55ef520f167b6b1b2f5f7a7bec361899f87b034b
John Sargeant <71139230+johnsargeant94@users.noreply.github.com>
2020-11-10 11:34:14 +0000

    Changes

--- public/basket.js
+++ public/basket.js
@@ -1,35 +1,54 @@
-let carts = document.querySelectorAll('#basketBTN');
+function addToBasket (event) {
+    console.log(event.target.dataset.id);
+} 
+
+console.log("hello")
+
+// let carts = document.querySelectorAll('#basketBTN');
+
+// let products = [
+//     {{#each products}}
+//     {
+        
+//         name: {{ this.name }},
+//         price: { { this.price } },
+//         inStock: { { this.inStock } },
+//         image: { { this.image } }
+//     },
+//     {{/each}}
+
+// ]
 
 
+// for (let i=0; i < carts.length; i++){
+//     carts[i].addEventListener('click', () => {
+//         cartNumbers(products[i]);
 
-for (let i=0; i < carts.length; i++){
-    carts[i].addEventListener('click', () => {
-        cartNumbers();
+//     })
+// }
 
-    })
-}
+// function onLoadCartNumbers() {
+//     let productNumbers = localStorage.getItem('cartNumbers');
 
-function onLoadCartNumbers() {
-    let productNumbers = localStorage.getItem('cartNumbers');
+//     if(productNumbers) {
+//         document.querySelector('basketNum').textContent = productNumbers;
+//     }
+// }
 
-    if(productNumbers) {
-        document.querySelector('basketNum').textContent = productNumbers;
-    }
-}
+// function cartNumbers(product) {
+//     console.log("the product clicked is", product);
+//     let productNumbers = localStorage.getItem('cartNumbers');
 
-function cartNumbers() {
-    let productNumbers = localStorage.getItem('cartNumbers');
+//     productNumbers = parseInt(productNumbers);
 
-    productNumbers = parseInt(productNumbers);
+//     if (productNumbers) {
+//         localStorage.setItem('cartNumbers' , productNumbers + 1);
+//         document.querySelector('basketNum').textContent = productNumbers + 1;
+//     } else {
+//         localStorage.setItem('cartNumbers' , 1);
+//         document.querySelector('basketNum').textContent = 1;
+//     }
 
-    if (productNumbers) {
-        localStorage.setItem('cartNumbers' , productNumbers + 1);
-        document.querySelector('basketNum').textContent = productNumbers + 1;
-    } else {
-        localStorage.setItem('cartNumbers' , 1);
-        document.querySelector('basketNum').textContent = 1;
-    }
+// }
 
-}
-
-onLoadCartNumbers()
+// onLoadCartNumbers()
