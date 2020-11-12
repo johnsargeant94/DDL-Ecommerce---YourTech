334fe0eced390030e7de56ddb1052f1495c4be3c
John Sargeant <71139230+johnsargeant94@users.noreply.github.com>
2020-11-12 11:27:10 +0000

    £ to show

--- public/basket.js
+++ public/basket.js
@@ -158,7 +158,7 @@ var shoppingCart = (function() {
     for(var i in cartArray) {
       output += "<tr>"
         + "<td>" + cartArray[i].name + "</td>" 
-        + "<td>(" + cartArray[i].price + ")</td>"
+        + "<td>(£" + cartArray[i].price + ")</td>"
         + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
         + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
         + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
